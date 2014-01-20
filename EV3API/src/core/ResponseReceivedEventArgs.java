fd7344dbc003566188fa61dff7a4dd5193ae5fa2
Jonathan Taws <jonathan.taws@gmail.com>
2014-01-20 21:22:59 +0100

    Implemented ResponseReceivedEventArgs

---
+++ EV3API/src/core/ResponseReceivedEventArgs.java
@@ -0,0 +1,18 @@
+package core;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class ResponseReceivedEventArgs {
+    // Byte array of the data received from the EV3 brick.
+    public byte[] report;
+
+    public byte[] get() {
+        return report;
+    }
+
+    public void setReport(byte[] report) {
+        this.report = report;
+    }
+}
