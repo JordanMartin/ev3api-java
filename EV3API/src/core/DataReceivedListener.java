3230d919cd9d26f1652c52312614f5c85b187a48
Jonathan Taws <jonathan.taws@gmail.com>
2014-01-23 23:49:48 +0100

    Small commit

--- EV3API/src/core/DataReceivedListener.java
+++ EV3API/src/core/DataReceivedListener.java
@@ -7,5 +7,5 @@ import java.util.EventListener;
  * @author Jonathan
  */
 public interface DataReceivedListener extends EventListener {
-    public void dataReceived();
+    public void dataReceived(byte[] data);
 }
